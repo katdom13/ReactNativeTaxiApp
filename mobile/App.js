e4f6caad09af37948eaa605aeed77d98a4c0272a
katdom13 <katdom13@gmail.com>
2021-05-19 17:19:11 +0800

    Get current location in react native

--- mobile/App.js
+++ mobile/App.js
@@ -1,24 +1,71 @@
-import React from 'react'
-import MapView from 'react-native-maps'
-import {StyleSheet} from 'react-native'
+import React, {useEffect, useState} from 'react'
+import MapView, {Marker} from 'react-native-maps'
+import {StyleSheet, View} from 'react-native'
+import Geolocation from '@react-native-community/geolocation'
 
 const App = () => {
+  const [latitude, setLatitude] = useState(0)
+  const [longitude, setLongitude] = useState(0)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    Geolocation.getCurrentPosition(
+      position => {
+        setLatitude(position.coords.latitude)
+        console.log('!!!', position.coords.latitude)
+        setLongitude(position.coords.longitude)
+        setError(null)
+      },
+      error => setError(error.message),
+      {
+        enableHighAccuracy: true,
+        timeout: 20000,
+        maximumAge: 2000,
+      },
+    )
+    // navigator.geolocation.getCurrentPosition(
+    //   position => {
+    //     setLatitude(position.coords.latitude)
+    //     setLongitude(position.coords.longitude)
+    //     setError(null)
+    //   },
+    //   error => setError(error.message),
+    //   {
+    //     enableHighAccuracy: true,
+    //     timeout: 20000,
+    //     maximumAge: 2000,
+    //   },
+    // )
+  }, [])
+
   return (
-    <MapView
-      style={styles.mapStyle}
-      initialRegion={{
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }}
-    />
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: latitude,
+          longitude: longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        <Marker
+          coordinate={{
+            latitude: latitude,
+            longitude: longitude,
+          }}
+        />
+      </MapView>
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
-  mapStyle: {
-    flex: 1,
+  container: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
   },
 })
 
