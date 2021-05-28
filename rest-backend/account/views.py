7a05137ab418eba107025586129b6efc11e79674
katdom13 <katdom13@gmail.com>
2021-05-28 20:19:43 +0800

    Override user create and update to hash password, allow partial updates, add username to serializer, validate user email to be unique

--- rest-backend/account/views.py
+++ rest-backend/account/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
+from rest_framework.response import Response
 
 from .serializers import UserSerializer
 
@@ -10,3 +11,13 @@ class UserViewSet(viewsets.ModelViewSet):
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    # Set partial updated equals to true to ignore
+    # required updates on fields that are not in the PUT request
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        return Response(serializer.data)
