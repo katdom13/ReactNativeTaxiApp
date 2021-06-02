32232ff7d0e1d5cec2c7976a4e0be55bf396b575
katdom13 <katdom13@gmail.com>
2021-06-02 19:07:44 +0800

    Updated Selection screen

--- mobile/screens/Selection.js
+++ mobile/screens/Selection.js
@@ -1,42 +1,62 @@
 import React from 'react'
-import {View, StyleSheet, TouchableOpacity, Text} from 'react-native'
+import {
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  Image,
+  Platform,
+} from 'react-native'
 import colors from '../config/colors'
 
 const Selection = ({navigation}) => {
   return (
     <View style={styles.container}>
       <TouchableOpacity
-        style={styles.button}
-        onPress={() => navigation.navigate('Passenger')}
-      >
-        <Text style={styles.buttonText}>Passenger</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.button}
+        style={[styles.choiceContainer, {borderBottomWidth: 1}]}
         onPress={() => navigation.navigate('Driver')}
       >
-        <Text style={styles.buttonText}>Driver</Text>
+        <Text style={styles.choiceText}>I'm a driver</Text>
+        <Image
+          source={require('../images/steeringwheel.png')}
+          style={styles.selectionImage}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.choiceContainer}
+        onPress={() => navigation.navigate('Passenger')}
+      >
+        <Text style={styles.choiceText}>I'm a Passenger</Text>
+        <Image
+          source={require('../images/passenger.png')}
+          style={styles.selectionImage}
+        />
       </TouchableOpacity>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  button: {
-    padding: 5,
-    marginTop: 5,
-    marginBottom: 5,
+  choiceContainer: {
+    flex: 1,
+    borderColor: colors.white,
+    justifyContent: 'center',
     alignItems: 'center',
   },
-  buttonText: {
-    color: colors.primary,
-    textTransform: 'uppercase',
-    fontSize: 18,
-    fontWeight: 'bold',
+  choiceText: {
+    color: colors.white,
+    fontSize: 32,
+    fontWeight: '200',
+    fontFamily: Platform.OS === 'android' ? 'sans-serif-light' : null,
+    marginBottom: 20,
   },
   container: {
     flex: 1,
-    marginTop: 50,
+    backgroundColor: '#3A3743',
+  },
+  selectionImage: {
+    height: 200,
+    width: 200,
   },
 })
 
